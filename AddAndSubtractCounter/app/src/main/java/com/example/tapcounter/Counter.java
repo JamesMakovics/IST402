d87dd693b159787269a1663757d8123d236cb40b
JamesMakovics <jmakovics1@gmail.com>
2021-09-13 12:12:08 -0400

    Homework 1
    Completed Homework 1

---
+++ AddAndSubtractCounter/app/src/main/java/com/example/tapcounter/Counter.java
@@ -0,0 +1,18 @@
+package com.example.tapcounter;
+
+public class Counter {
+    private int mCount;
+
+    public Counter(){
+        mCount = 0;
+    }
+    public void addCount(){
+        mCount++;
+    }
+    public void subCount(){
+        mCount--;
+    }
+    public Integer getCount(){
+        return mCount;
+    }
+}
